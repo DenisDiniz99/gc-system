67f19236d9f386d3dffd5911b30aa44b14ebfc90
PrefCapetinga <ti@capetinga.mg.gov.br>
2021-11-16 15:52:46 -0300

    Verificação de usuário administrador por meio do token para permitir ações de criar, atualizar e deletar clientes

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -16,7 +16,7 @@ exports.getAll = async(req, res) => {
         let data = await service.getAll();
         //Se existir clientes cadastrados retorna os mesmos
         //Senão envia uma mensagem de clientes não encontrados
-        if(data.length > 0){
+        if(data !== undefined && data !== null){
             res.status(200).send(data);
         }else{
             res.status(400).send({
@@ -34,11 +34,16 @@ exports.getAll = async(req, res) => {
 //Função para retornar clientes com status ativo
 exports.getByActiveStatus = async(req, res) => {
     try{
+        //Faz a chamada da função que verifica se existe token
+        //Caso exista, verifica se ele é válido
+        //Caso seja válido, libera a busca de clientes por status ativo
         jwt.authorize(req, res);
-
+        //Caso exista um token válido
+        //Faz-se a busca por clientes
         let data = await service.getByActiveStatus();
-
-        if(data.length > 0){
+        //Se existir clientes cadastrados retorna os mesmos
+        //Senão envia uma mensagem na aviso
+        if(data !== undefined && data !== null){
             res.status(200).send(data);
         }else{
             res.status(400).send({
@@ -58,9 +63,10 @@ exports.getById = async(req, res) => {
     try{
         jwt.authorize(req, res);
 
-        var data = await service.getById(req.params.id);
+        let data = await service.getById(req.params.id);
+        console.log(data);
 
-        if(data.length > 0){
+        if(data !== undefined && data !== null){
             res.status(200).send(data);
         }else{
             res.status(400).send({
@@ -82,14 +88,19 @@ exports.create = async(req, res) => {
         //E sendo válido, se o usuário possui direitos de admin
         jwt.isAdmin(req, res);
 
+        //Verifica se existem erros na Model
         let {errors} = validationResult(req);
-        
+        //Caso exista algum erro na Model
+        //Retorna um Json com os erros
         if(errors.length > 0){
             return res.status(400).send({ message: errors });
         }
-
+        //Se não houver erros tenta criar um novo cliente
         let result = await service.create(req.body);
-        if(result !== undefined){
+        //Se ocorrer a criação com sucesso
+        //Retorna um status 201
+        //Senão retorna um status 400
+        if(result !== undefined && result !== null){
             res.status(201).send( {
                 message: "Cliente cadastrado com sucesso"
             });
@@ -111,14 +122,23 @@ exports.create = async(req, res) => {
 //Função para atualizar dados do cliente
 exports.update = async(req,res) => {
     try{
+        //Verifica se o usuário possui um token válido
+        //E sendo válido, se o usuário possui direitos de admin
+        jwt.isAdmin(req, res);
+        //Verifica se existem erros na Model
         let {errors} = validationResult(req);
-
+        //Caso exista algum erro na Model
+        //Retorna um Json com os erros
         if(errors.length > 0){
             return res.status(400).send({ message: errors });
         }
-
+        //Se não houver erros
+        //Tenta atualizar os dados do cliente
         let result = await service.update(req.params.id, req.body);
-        if(result != undefined){
+        //Se ocorrer a atualização com sucesso
+        //Retorna um status 200
+        //Senão retorna um status 400
+        if(result !== undefined && result !== null){
             res.status(200).send({
                     essage: "Cliente atualizado com sucesso"
             });
@@ -138,14 +158,23 @@ exports.update = async(req,res) => {
 //Função para deletar um cliente
 exports.delete = async(req, res) => {
     try{
+        //Verifica se o usuário possui um token válido
+        //E sendo válido, se o usuário possui direitos de admin
+        jwt.isAdmin(req, res);
+        //Verifica se existem erros na Model
         let {errors} = validationResult(req);
-        
+        //Caso exista algum erro na Model
+        //Retorna um Json com os erros
         if(errors.length > 0){
             return res.status(400).send({ message: errors });
         }
-
+        //Se não houver erros
+        //Tenta excluir o cliente
         let result = await service.delete(req.params.id);
-        if(result != undefined){
+        //Se ocorrer a exclusão com sucesso
+        //Retorna um status 200
+        //Senão retorna um status 400
+        if(result !== undefined && result !== null){
             res.status(200).send({
                 message: "Cliente excluído com sucesso"
             });
--- src/services/Auth/Auth.js
+++ src/services/Auth/Auth.js
@@ -13,7 +13,7 @@ exports.decodedToken = async(token) => {
     return data;
 }
 
-exports.authorize = function(req, res, next) {
+exports.authorize = function(req, res) {
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
     if(!token){
@@ -21,10 +21,14 @@ exports.authorize = function(req, res, next) {
             message: 'Acesso restrito'
         });
     } else {
-        jwt.verify(token, KEY , function(error, decoded) {
+        jwt.verify(token, KEY , function(error) {
+            //Faz uma verificação em busca de erros
+            //Se houver como por exemplo um token inválido
+            //Retorna a mensagem de Acesso inválido ou sessão expirada
+            //Senão retorna um status 200
             if(error){
                 res.status(401).json({
-                    message: 'Token inválido'
+                    message: 'Acesso inválido ou sessão expirada'
                 });
             }else{
                 res.status(200);
@@ -33,21 +37,30 @@ exports.authorize = function(req, res, next) {
     }
 }
 
+
 exports.isAdmin = function(req, res){
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
+    //Verifica se existe um token na requisição do client
+    //Se não existir retorna um status 401
     if(!token){
         res.status(401).json({
             message: 'Acesso restrito'
         });
     } else{
+        //Faz uma verificação no token repassado na requisição do client
         jwt.verify(token, KEY , function(error, decoded){
+            
+            //Se houver, como por exemplo um token inválido
+            //Retorna um status 401 com a mensagem de Acesso inválido ou sessão expirada
+            //Senão, verifica se o token possui uma role admin
+            //Se existir, retorna um status 200
+            //Senão, retorna um status 403
             if(error){
                 res.status(401).json({
-                    message: 'Token inválido'
+                    message: 'Acesso inválido ou sessão expirada'
                 });
             }else{
-                if(decoded.roles.include('admin')){
+                if(decoded.role.includes('admin')){
                     res.status(200);
                 }else{
                     res.status(403).json({
